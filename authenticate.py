063f9e0a75dff6be46b9882774ee89a9df9c6b2d
jthomson264 <jthomson264@gmail.com>
2014-04-23 20:01:42 -0400

    Added makeNewUser and Authenticate, not finalized
    Neither of these are functional yet

--- authenticate.py
+++ authenticate.py
@@ -1,9 +1,10 @@
 import sqlite3
 def authenticate(userPass,SSN):
+    salt = "1234567654321234565432 this is a salt 23961" 
     conn = sqlite3.connect('example.db')
     c = conn.cursor()
     c.execute("SELECT * FROM hashes WHERE ssn = '%s'" % SSN)
-    if hash(userPass) == c.fetchone():
+    if hash(userPass + salt) == c.fetchone():
         return True
     else:
         return False
